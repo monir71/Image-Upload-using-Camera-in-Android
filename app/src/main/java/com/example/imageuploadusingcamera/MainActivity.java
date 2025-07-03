bb00580ad6b092776b3aa72a253c3d602d0ad3bd
monir71 <zamanmonir71@yahoo.com>
2025-07-03 15:10:54 +0600

    just intial

---
+++ app/src/main/java/com/example/imageuploadusingcamera/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.imageuploadusingcamera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private final int CAMERA_REQ_CODE = 100;
+    ImageView imgCamera;
+    Button imgBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        imgCamera = findViewById(R.id.imgCamera);
+        imgBtn = findViewById(R.id.imgBtn);
+
+        imgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(iCamera, CAMERA_REQ_CODE);
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK)
+        {
+            if(requestCode == CAMERA_REQ_CODE)
+            {
+                Bitmap img = (Bitmap) data.getExtras().get("data");
+                imgCamera.setImageBitmap(img);
+            }
+        }
+
+    }
+}
